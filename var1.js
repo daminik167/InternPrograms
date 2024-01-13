44b814c8e2450afd0ed4399602c052fd35e97dfe
daminik167 <daminikarvande16@gmail.com>
2024-01-14 00:30:47 +0530

    Adding JS Var program

---
+++ var1.js
@@ -0,0 +1,11 @@
+//var keyword examples
+console.log("first example");
+var a=10
+function f(){
+    console.log(a)
+}
+f();
+console.log(a);
+
+//Output will be 10 10
+//beacause var has global and functional scope
